df58ab4c5ae4340078f917bfda296123f4e623cf
at-luanhuynh <luan.huynh@asiantech.vn>
2019-12-25 16:46:13 +0700

    DailyReport2512

---
+++ at_LuanHuynh/calculation.rb
@@ -0,0 +1,9 @@
+﻿def calculation(a,b)
+   
+    yield(a,b)
+    
+end 
+calculation(3,2) {|a, b| puts " Tong: #{a + b}"}
+calculation(3,2) {|a, b| puts "Hieu: #{a - b}"}
+calculation(3,2) {|a, b| puts " Tich: #{a * b}"}
+calculation(3,2) {|a, b| puts " Thuong #{a / b} "}
