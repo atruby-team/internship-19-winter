7f7c5c90704a1a3262914fa726ebe40fad334932
at-luanhuynh <luan.huynh@asiantech.vn>
2019-12-27 09:56:21 +0700

    Bai1,2,3,6.1

---
+++ at_LuanHuynh/Bai1.rb
@@ -0,0 +1,18 @@
+﻿def input
+    puts "Nhap vao n (0<= n <= 10): "
+    return n = gets.chomp.to_i
+end
+n = input
+if (n < 0) || (n > 10)
+    input
+elsif n >= 9
+    print "A"
+elsif n >= 8
+    print "B"
+elsif n >= 7
+    print "C"
+elsif n >= 6
+    print "D"
+else print "F"
+end  
+
---
+++ at_LuanHuynh/bai6_1.rb
@@ -0,0 +1,11 @@
+﻿def sum(arr)
+  s = 0
+  arr.each do |i|
+    s += i
+  end
+  yield(s)
+end
+a = [1, 2, 3, 1]
+sum(a) {|s| puts s}
+
+
