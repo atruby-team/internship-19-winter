9a0e6a663e1ddeeeaced96514118d1e778c9a223
at-luanhuynh <luan.huynh@asiantech.vn>
2020-01-08 14:29:50 +0700

    W3D1/at-luanhuynh

---
+++ at_LuanHuynh/W3D1/bai1.rb
@@ -0,0 +1,33 @@
+﻿class Student
+  attr_accessor :first_name, :last_name, :course
+  def def initialize
+    @first_name = ""
+    @last_name = ""
+    @course = []
+  end
+  def full_name
+    puts @first_name +" " + @last_name
+  end
+  def add_cours(course)
+    @course ||= []
+    @course << course
+  end
+  def show
+    print @course
+  end
+  def remove_course(course)
+    @course.delete(course)
+  end
+end
+student = Student.new 
+student.first_name = "Luan"
+p student.first_name
+student.last_name = "Huynh"
+p student.last_name
+student.full_name
+student.add_cours("Toan")
+student.add_cours("Ly")
+student.show
+student.remove_course("Ly")
+student.add_cours ("Programming")
+student.show
---
+++ at_LuanHuynh/W3D1/bai2.rb
@@ -0,0 +1,62 @@
+﻿class Dictionary
+  attr_accessor :vocab, :desc
+  def initialize
+    @hash = Hash.new
+  end
+  def hash
+    @hash
+  end
+  def add(vocab,desc) 
+    if @hash[vocab].nil?
+      @hash.store(vocab,desc)
+    end
+  end
+  def lookup(vocab) 
+    if  @hash[vocab] !=nil
+      p item = @hash[vocab]
+    else 
+      p "#{vocab} not found"
+    end
+  end
+  def remove(vocab)
+    if  @hash.has_key?(vocab) == true
+      @hash.delete(vocab)
+      p "removed #{vocab}"
+    else
+      p "#{vocab} not found"
+    end
+  end
+  def size
+    p "total vocabulary: #{@hash.length}"
+  end
+  def update(vocab,desc)
+    if  @hash.has_key?(vocab) == true
+      @hash.delete(vocab)
+      add(vocab,desc)
+      p "updated #{vocab}"
+    else
+      return nil
+    end
+  end
+  def pop
+    @hash.delete(@hash.key(@hash.values.last))
+    p "Removed newest"
+  end
+end
+dic = Dictionary.new
+dic.add("hello", "xin chao")
+dic.add("good bye", "tam biet")
+dic.add("sleep", "ngu")
+dic.add("eat", "an")
+p dic.hash
+dic.remove("sleep") #"removed sleep"
+dic.remove("sleep") #"sleep not found"
+p dic.hash
+dic.lookup("hello") #"xin chao"
+dic.size #"total vocabulary: 3"
+dic.update("good bye", "Chao tam biet") #"updated good bye"
+p dic.hash
+dic.pop
+dic.pop
+dic.pop
+dic.size #"total vocabulary: 0"
