13fc77cbd0f819c937af7c0980b56526bf23a560
at-luanhuynh <luan.huynh@asiantech.vn>
2020-01-08 08:22:30 +0700

    finish

---
+++ at_LuanHuynh/W1D1/bai3.rb
@@ -0,0 +1,9 @@
+﻿def maximum(arr)
+    arr.sort!
+    new_arr = arr.last(3)
+    s = 1
+    new_arr.each {|val| s *= val}
+    print s
+end
+arr= [-1, -2, 2, 3, 4]
+maximum(arr)
---
+++ at_LuanHuynh/W1D1/bai5.rb
@@ -0,0 +1,6 @@
+﻿arr = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+h = Hash.new(0)
+arr.each do |val|
+  h[val] += 1
+end
+print h 
