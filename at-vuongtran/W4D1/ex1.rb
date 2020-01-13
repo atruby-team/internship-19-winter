426edfac21cc0627bf622a1c4056e5cc2b6205da
at-vuongtran <vuong.tran@asiantech.vn>
2020-01-13 16:38:22 +0700

    done ex1 caculator sum, div, multi, sub

---
+++ at-vuongtran/W4D1/ex1.rb
@@ -0,0 +1,37 @@
+def add_number(a, b, *n)
+  proc = Proc.new { a + b + n.sum }
+  proc
+end
+
+def sub_number(a, b, *n)
+  proc = Proc.new { a - b - n.sum }
+  proc
+end
+
+def mul_number(a, b, *n)
+  proc = Proc.new { a * b * n.reduce(:*) }
+  proc
+end
+
+def div_number(a, b, *n)
+  proc = Proc.new do
+    begin
+      a / (b * n.reduce(:*))
+    rescue => exception
+      "can not div by zero"
+    end
+  end 
+  proc
+end
+
+add = add_number(1, 2, 3)
+p add.call
+
+sub = sub_number(1, 2, 3, 4)
+p sub.call
+
+mul = mul_number(1, 2, 3, 4)
+p mul.call
+
+div = div_number(10, 1, 3)
+p div.call
