ee35dd6955da53cd0d6e594cba3dbe25540e9668
at-luanhuynh <luan.huynh@asiantech.vn>
2020-01-20 16:53:11 +0700

    Dang lam bai 2

---
+++ at_LuanHuynh/W4D1/bai2.rb
@@ -0,0 +1,17 @@
+﻿def add_more(x)
+  add = -> (y) { x + y }
+  add
+end
+
+def multi_more(x)
+  multi = -> (y) { x * y }
+  multi
+end
+
+def add_multi(x,y)
+  
+end
+add4 = add_more(4)
+p add4.(5)
+multi10 = multi_more(10)
+p multi10.(10)
