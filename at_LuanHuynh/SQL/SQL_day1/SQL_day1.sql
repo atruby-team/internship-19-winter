b3b9a265aad0a6e430c3c605f36befecaccc1386
at-luanhuynh <luan.huynh@asiantech.vn>
2020-02-03 09:26:01 +0700

    SQL_day1

---
+++ at_LuanHuynh/SQL/SQL_day1/SQL_day1.sql
@@ -0,0 +1,12 @@
+﻿SELECT * FROM MySQL_intern.books;
+
+Select name, email from MySQL_intern.users U left outer join MySQL_intern.books B on U.id = B.id_user
+where book = 'naruto';
+
+Select id_user, name, count(id_user) as Book_count
+from MySQL_intern.users U inner join MySQL_intern.books B on U.id = B.id_user
+group by name, id_user;
+
+Select name, count(id_user) as Book_count
+from MySQL_intern.users U inner join MySQL_intern.books B on U.id = B.id_user
+group by id_user 
